4a9ea8253cb2397b20a940476d13a11c5c123246
rpratama94 <rpratama.sky@gmail.com>
2020-11-13 09:20:02 +0700

    INtegrated Socket server-side

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,7 @@
 const routes = require('express').Router()
-const controller = require('../controllers/controlGet')
 
-routes.get('/soals', controller.getAllSoal)
-routes.post('/rooms', controller.createRoom)
-routes.post('/users', controller.createUser)
-routes.post('/join', controller.joinRoom)
-routes.get('/rooms', controller.getAllRooms)
-routes.delete('/roomUser', controller.leaveRoom)
-routes.get('/roomDetail/:roomName', controller.getDetailRoomByName)
-routes.get('/rooms/:name/users', controller.getAllUsers)
+routes.get('/', (req, res, next) => {
+  res.status(200).json({message: 'Connected'})
+})
+
 module.exports = routes
